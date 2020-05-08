66f490e0179e26452118a7b680d65bcb7ef2b4bb
Chuckrutis <renan.arantes@rede.ulbra.br>
2020-05-08 13:38:24 -0300

    Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Name</h1>
+    <h2>Profission</h2>
+    <p>
+      Bio BioBioBioBioBioBioBio
+      Bio BioBioBioBioBioBioBio
+    </p>
+  </div>
+)
+
+export default Profile
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 const AboutPage = () => (
-  <>
+  <Layout>
+    <SEO title="About"/>
     <h1>About Page</h1>
     <Link to="/about" activeStyle={{color:'red'}}>About</Link>
     <Link to="/" >Home</Link>
-  </>
+  </Layout>
 )
 
 export default AboutPage
