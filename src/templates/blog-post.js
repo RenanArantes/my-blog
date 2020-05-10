607676cb60b7f3c9cb2fde858d22dabe5ca9050b
RenanArantes <renan.arantes@rede.ulbra.br>
2020-05-10 10:40:19 -0300

    Ordenando pela data - 7 Estruturando a home

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 const BlogPost = ({ data }) => {
-
   const post = data.markdownRemark
 
   return(
