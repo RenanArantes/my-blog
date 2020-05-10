16cb1e0fd4331457b0cf9e28cbb2fa4a5f331281
RenanArantes <renan.arantes@rede.ulbra.br>
2020-05-10 17:18:45 -0300

    Template home - 7 Estruturando a home

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,25 +22,24 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   }
 }
 
-exports.createPages = ({graphql, actions}) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
-
   return graphql(`
-    query PostList {
-      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+    {
+      allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
         edges {
           node {
-            timeToRead
-            frontmatter {
-              background
-              category
-              date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
-              description
-              title
-            }
             fields {
               slug
             }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            timeToRead
           }
         }
       }
@@ -51,28 +50,29 @@ exports.createPages = ({graphql, actions}) => {
     posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
-        component: path.resolve("./src/templates/blog-post.js"),
+        component: path.resolve(`./src/templates/blog-post.js`),
         context: {
-          slug: node.fields.slug
-        }
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
       })
     })
 
-    const postsPerPage = 4
+    const postsPerPage = 6
     const numPages = Math.ceil(posts.length / postsPerPage)
 
     Array.from({ length: numPages }).forEach((_, index) => {
       createPage({
-        path: index === 0 ? '/' : `/page/${index + 1}`,
-        component: path.resolve("./src/templates/blog-list.js"),
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
         context: {
           limit: postsPerPage,
           skip: index * postsPerPage,
           numPages,
-          currentPage: index + 1
-        }
+          currentPage: index + 1,
+        },
       })
     })
   })
-
 }
