985e217a039b6cf4c5896d46279e7a675d331564
Chuckrutis <renan.arantes@rede.ulbra.br>
2020-05-11 14:46:22 -0300

    adicionando estilos bases para o post - 8 Estruturando a Pagina de Post

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -6,7 +6,8 @@ export const LayoutWrapper = styled.section`
 /** VH = view height, pega a altura da tela 100 e igual a 100% */
 export const LayoutMain = styled.main`
   background: #16202c;
-  min-height: 100vh; 
+  min-height: 100vh;
+  transition: background, color 0.5s;
   padding: 0 3.75ren 0 20rem;
   width: 100%;
 `
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,15 @@ import { graphql } from 'gatsby'
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
+import {
+  PostWrapper,
+  PostHeader,
+  PostTitle,
+  PostDescription,
+  PostDate,
+  MainContent,
+} from "../components/Post/styles"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
@@ -11,7 +20,18 @@ const BlogPost = ({ data }) => {
     <>
       <Layout>
         <SEO title={post.frontmatter.title} />
-        <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+        <PostWrapper>
+          <PostHeader>
+            <PostDate>
+              {post.frontmatter.date} - {post.timeToRead} min de leitura
+            </PostDate>
+            <PostTitle> {post.frontmatter.title} </PostTitle>
+            <PostDescription> {post.frontmatter.description} </PostDescription>
+          </PostHeader>
+          <MainContent>
+            <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+        </MainContent>
+        </PostWrapper>
       </Layout>
     </>
   )
@@ -23,8 +43,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
