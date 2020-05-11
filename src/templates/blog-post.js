1d56536493d09d2c8692c31dab40098f8f29c9ee
Chuckrutis <renan.arantes@rede.ulbra.br>
2020-05-11 14:19:22 -0300

    integrando o layout ao blogpost - 8 Estruturando a pagina de post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return(
     <>
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+      <Layout>
+        <SEO title={post.frontmatter.title} />
+        <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+      </Layout>
     </>
   )
 
