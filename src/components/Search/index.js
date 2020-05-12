9bc0ff3fdd3bf063f505caf47fa82ed70ddf8c1c
Chuckrutis <renan.arantes@rede.ulbra.br>
2020-05-12 09:46:07 -0300

    Search page and component base -  9 Search

---
+++ src/components/Search/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { SearchWrapper } from './styles'
+
+const Search = () => (
+  <SearchWrapper>
+    <h1>Search Page</h1>
+  </SearchWrapper>
+)
+
+export default Search
---
+++ src/pages/search.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import Search from "../components/Search"
+
+const SeachPage = () => (
+  <Layout>
+    <SEO title="Seach"/>
+    <Search>
+
+    </Search>
+  </Layout>
+)
+
+export default SeachPage
