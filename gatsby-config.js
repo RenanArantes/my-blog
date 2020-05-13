51ae9340689128c0355f3562f547ec6b0c1fbaf1
Renan <malevo18@gmail.com>
2020-05-13 10:56:37 -0300

    offline plugin - 12 PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,9 +87,9 @@ module.exports = {
         icon: `static/assets/img/blog-icon-test.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sitemap`
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
